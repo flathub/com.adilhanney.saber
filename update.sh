36be88efdb138afa2cb2d16466e0067e623fa2e2
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-12 20:04:30 +0000

    update.sh: update for new filename format

--- update.sh
+++ update.sh
@@ -6,9 +6,11 @@ git checkout master
 git fetch origin
 git reset --hard origin/master
 
-echo -n "2. Finding latest tag... "
-export LATEST_TAG=$(curl -s https://api.github.com/repos/adil192/saber/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-echo "Found $LATEST_TAG"
+echo -n "2. Finding latest tag and archive... "
+json=$(curl -s https://api.github.com/repos/adil192/saber/releases/latest)
+export LATEST_TAG=$(echo $json | jq -r ".tag_name")
+export ARCHIVE_NAME=$(echo $json | jq -r ".assets[].name" | grep Saber_.*.tar.gz)
+echo "Found $LATEST_TAG and $ARCHIVE_NAME"
 
 echo -n "3. Check if json needs updating... "
 if grep -q "adil192/saber/$LATEST_TAG" com.adilhanney.saber.json; then
@@ -18,10 +20,10 @@ else
     echo "Yes"
 fi
 
-echo -n "4. Hashing Saber-Linux-Portable.tar.gz..."
-wget https://github.com/adil192/saber/releases/latest/download/Saber-Linux-Portable.tar.gz
-export ARCHIVE_HASH=$(sha256sum Saber-Linux-Portable.tar.gz | cut -d ' ' -f 1)
-rm Saber-Linux-Portable.tar.gz
+echo -n "4. Hashing $ARCHIVE_NAME... "
+wget https://github.com/adil192/saber/releases/latest/download/$ARCHIVE_NAME
+export ARCHIVE_HASH=$(sha256sum $ARCHIVE_NAME | cut -d ' ' -f 1)
+rm $ARCHIVE_NAME
 echo "Hashed $ARCHIVE_HASH"
 
 echo -n "5. Hashing com.adilhanney.saber.metainfo.xml"
@@ -33,8 +35,10 @@ echo "Hashed $METAINFO_HASH"
 echo -n "6. Updating com.adilhanney.saber.json... "
 # com.adilhanney.saber.metainfo.xml
 sed -i "s/\"url\": \"https:\/\/raw.githubusercontent.com\/adil192\/saber\/v[0-9]\.[0-9]\.[0-9]\/flatpak\/com.adilhanney.saber.metainfo.xml\", \"sha256\": \"[a-z0-9]\{64\}\"/\"url\": \"https:\/\/raw.githubusercontent.com\/adil192\/saber\/$LATEST_TAG\/flatpak\/com.adilhanney.saber.metainfo.xml\", \"sha256\": \"$METAINFO_HASH\"/g" com.adilhanney.saber.json
-# Saber-Linux-Portable.tar.gz
-sed -i "s/\"url\": \"https:\/\/github.com\/adil192\/saber\/releases\/download\/v[0-9]\.[0-9]\.[0-9]\/Saber-Linux-Portable.tar.gz\", \"sha256\": \"[a-z0-9]\{64\}\"/\"url\": \"https:\/\/github.com\/adil192\/saber\/releases\/download\/$LATEST_TAG\/Saber-Linux-Portable.tar.gz\", \"sha256\": \"$ARCHIVE_HASH\"/g" com.adilhanney.saber.json
+# Replace Saber-Linux-Portable.tar.gz (old archive name)
+sed -i "s/\"url\": \"https:\/\/github.com\/adil192\/saber\/releases\/download\/v[0-9]\.[0-9]\.[0-9]\/Saber-Linux-Portable.tar.gz\", \"sha256\": \"[a-z0-9]\{64\}\"/\"url\": \"https:\/\/github.com\/adil192\/saber\/releases\/download\/$LATEST_TAG\/$ARCHIVE_NAME\", \"sha256\": \"$ARCHIVE_HASH\"/g" com.adilhanney.saber.json
+# Replace Saber_v0.9.2_9020.tar.gz ($ARCHIVE_NAME)
+sed -i "s/\"url\": \"https:\/\/github.com\/adil192\/saber\/releases\/download\/v[0-9]\.[0-9]\.[0-9]\/Saber_v[0-9]\.[0-9]\.[0-9]_[0-9]\{4\}\.tar\.gz\", \"sha256\": \"[a-z0-9]\{64\}\"/\"url\": \"https:\/\/github.com\/adil192\/saber\/releases\/download\/$LATEST_TAG\/$ARCHIVE_NAME\", \"sha256\": \"$ARCHIVE_HASH\"/g" com.adilhanney.saber.json
 echo "Done"
 
 echo -n "7. Committing changes... "
