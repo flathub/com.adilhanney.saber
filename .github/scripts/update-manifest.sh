4ea4e144fca4b54324e45e611a36143224124398
Adil Hanney <adilhanney@disroot.org>
2026-03-21 02:20:52 +0000

    Update Saber to v1.32.0

--- .github/scripts/update-manifest.sh
+++ .github/scripts/update-manifest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v "$PWD":/usr/src/flatpak -u `id -u`:`id -g` theappgineer/flatpak-flutter:latest flatpak-flutter.yaml
+docker run --rm --pull=always -v "$PWD":/usr/src/flatpak -u `id -u`:`id -g` theappgineer/flatpak-flutter:latest flatpak-flutter.yaml
